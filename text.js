f4c5641c4f66ef11c05936a514be3d36028f9608
MaxwellEngineer <amirgood99910.com@gmail.com>
2026-03-10 18:02:32 +0500

    Update text.js

--- text.js
+++ text.js
@@ -1,1 +1,13 @@
-console.log("Скрипт успешно подключен!");
+const btn = document.getElementById("theme-toggle");
+
+btn.addEventListener("click", function() {
+    document.body.classList.toggle("dark-theme");
+
+    if (document.body.classList.contains("dark-theme")) {
+        btn.textContent = "Светлая тема";
+    } else {
+        btn.textContent = "Темная тема";
+    }
+});
+
+console.log("Скрипт переключения темы готов!");
